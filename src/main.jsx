f6965532db9c144e273fe1f54782525604e89130
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-04-27 13:39:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 import { useContext } from 'react'
+import Ibutton from './Ibutton/Ibutton';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
     <StoreContextProvider>
       <App />
+      <Ibutton />
     </StoreContextProvider>
   </BrowserRouter>
 
